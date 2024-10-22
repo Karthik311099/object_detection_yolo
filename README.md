9d3c1655995083f1e5708af56ceeb408dd0408e3
karthik311099 <karthikm3110@hotmail.com>
2024-10-22 16:00:03 +0530

    setup file updated

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # object_detection_yolo
+
+## How to run:
+
+```bash
+conda create -n obj python=3.7 -y
+```
+
+```bash
+conda activate obj
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python app.py
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguagesDetection', 
+    version= '0.0.1',
+    author= 'karthik',
+    author_email= 'karthik@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
